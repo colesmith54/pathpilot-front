44b040e3e9e43c98fff84cef0ae7ddf47d6ac473
Cole Smith <114085318+colesmith54@users.noreply.github.com>
2024-03-20 19:10:26 +0000

    path

--- src/App.tsx
+++ src/App.tsx
@@ -170,8 +170,8 @@ function App() {
                   </Pin>
                 </AdvancedMarker>
               ))}
-              <Polygon strokeWeight={1.5} encodedPaths={encodePolyline([dijkstraRoute])} />
-              <Polygon strokeWeight={1.5} encodedPaths={encodePolyline([aStarRoute])} />
+              <Polygon strokeWeight={1.5} encodedPaths={[encodePolyline(dijkstraRoute)]} />
+              <Polygon strokeWeight={1.5} encodedPaths={[encodePolyline(aStarRoute)]} />
             </Map>
           </div>
           <div className={'w-4/12 h-5/6 mx-20'}>
