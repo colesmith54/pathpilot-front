e20d564194361dc0b6d9f1f40af51d0c71474db7
Cole Smith <114085318+colesmith54@users.noreply.github.com>
2024-03-20 03:48:11 +0000

    deploy

--- src/api/markerApi.tsx
+++ src/api/markerApi.tsx
@@ -9,11 +9,11 @@ type Marker = {
 const fetchMarkerInfo = async (marker: Marker) => {
     try {
         if (!marker.placeId) {
-            const response = await axios.get('/api/nearest', { params: marker });
+            const response = await axios.get('https://pathpilot-back.vercel.app/api/nearest', { params: marker });
             marker.placeId = response.data.place_id;
             marker.address = response.data.formatted_address;
         }
-        const response = await axios.get('/api/marker', { params: marker });
+        const response = await axios.get('https://pathpilot-back.vercel.app/api/marker', { params: marker });
         marker.address = response.data.result.formatted_address;
         return marker;
     } catch (error) {
@@ -24,7 +24,7 @@ const fetchMarkerInfo = async (marker: Marker) => {
 
 const findRoute = async (markers: Marker[], setDijkstraRoute: (value: google.maps.LatLngLiteral[]) => void, setAStarRoute: (value: google.maps.LatLngLiteral[]) => void) => {
     try {
-        const response = await axios.get('/api/route', { params: { start: markers[0], end: markers[1] } });
+        const response = await axios.get('https://pathpilot-back.vercel.app/api/route', { params: { start: markers[0], end: markers[1] } });
         const { dijkstra, aStar } = response.data;
         console.log(dijkstra, aStar);
         setDijkstraRoute(dijkstra);
