2c4b6cbc9dc3728d9b495dcf69b7556c3e1e122c
Cole Smith <114085318+colesmith54@users.noreply.github.com>
2024-03-20 19:49:31 +0000

    path

--- src/components/ui/polygon.tsx
+++ src/components/ui/polygon.tsx
@@ -74,6 +74,7 @@ import {
       const paths = encodedPaths.map(path =>
         geometryLibrary.encoding.decodePath(path)
       );
+      console.log(paths)
       polygon.setPaths(paths);
     }, [polygon, encodedPaths, geometryLibrary]);
   
