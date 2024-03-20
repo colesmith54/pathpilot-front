87b57e696420ec1b9eb3e8f76458a546a87fe87d
Cole Smith <114085318+colesmith54@users.noreply.github.com>
2024-03-20 19:36:04 +0000

    path

--- src/App.tsx
+++ src/App.tsx
@@ -67,6 +67,7 @@ function encodePolyline(points: google.maps.LatLngLiteral[] | undefined): string
     const temp = point.lat;
     point.lat = point.lng.toFixed(5);
     point.lng = point.lat.toFixed(5);
+  }
   const result = google.maps.geometry.encoding.encodePath(points)
   return result;
 }
