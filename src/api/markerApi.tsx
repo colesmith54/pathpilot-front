ede76a09f76509059bfc81fce11290abdb8e882f
Cole Smith <114085318+colesmith54@users.noreply.github.com>
2024-04-21 18:30:20 +0000

    add stop logic

--- src/api/markerApi.tsx
+++ src/api/markerApi.tsx
@@ -24,8 +24,11 @@ const fetchMarkerInfo = async (marker: Marker) => {
 
 const findRoute = async (markers: Marker[], setDijkstraRoute: (value: google.maps.LatLngLiteral[]) => void, setAStarRoute: (value: google.maps.LatLngLiteral[]) => void, setBfsRoute: (value: google.maps.LatLngLiteral[]) => void, setDijkstraTime: (value: number) => void, setAStarTime: (value: number) => void, setBfstime: (value: number) => void) => {
     try {
-        const response = await axios.get('https://pathpilot-back.vercel.app/api/route', { params: { start: markers[0], end: markers[1] } });
-        const { dijkstra, aStar, dijkstraTime, aStarTime, bfs, bfsTime } = response.data;
+        const response = markers.length > 2 ? 
+            await axios.get('https://pathpilot-back.vercel.app/api/route', { params: { start: markers[0], mid: markers[1], end: markers[1] } }) :
+            await axios.get('https://pathpilot-back.vercel.app/api/route', { params: { start: markers[0], end: markers[1] } });
+        let { dijkstra, aStar, dijkstraTime, aStarTime, bfs, bfsTime } = response.data;
+
         setDijkstraRoute(dijkstra);
         setAStarRoute(aStar);
         setBfsRoute(bfs);
